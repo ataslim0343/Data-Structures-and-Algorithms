cb78b8c5381ca5bd2d13073b70f8012ee5b79e7d
Md Taslim Ansari <ataslim0343@gmail.com>
2020-06-05 02:38:36 +0530

    Update Key_Pair.py

--- GeeksForGeeks/Array/Key_Pair.py
+++ GeeksForGeeks/Array/Key_Pair.py
@@ -1,6 +1,17 @@
 #Given an array A of N positive integers and another number X. 
 #Determine whether or not there exist two elements in A whose sum is exactly X.
 
+
+#Explanation -
+
+'''
+Given arr=[2,3,4,5] and x=9
+suppose there is a pair (a,b) such that a+b=x, it implies that b=x-a given that a!=b.
+Note - If there are duplicate elements in array then index(a)!=index(b).
+We are using the same idea here in the solution below.
+
+'''
+
 #Code- Considering Dupliate Elements as well
 
 for t in range(int(input())):
@@ -8,7 +19,6 @@ for t in range(int(input())):
     arr=[int(i) for i in input().split()]
     flag=0
     for i in range(n):
-#x=arr[i] + k ==>> if k is in array then the pair exist.
         k=x-arr[i]
         if k in arr and arr.index(k)!=i:
             flag=1
@@ -17,6 +27,8 @@ for t in range(int(input())):
         print("Yes")
     else:
         print("No")
+
+
             
     
     
