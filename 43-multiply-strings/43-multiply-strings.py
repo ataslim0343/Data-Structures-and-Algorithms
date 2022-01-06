4a37667ff4ac7da60befe7887ae9b7e68371b3b3
Md Taslim Ansari <ataslim0343@gmail.com>
2022-01-07 01:23:20 +0530

    Time: 32 ms (55.35%), Space: 13.7 MB (26.95%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -5,12 +5,24 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        if num1 == '0' or num2 == '0':
+            return '0'
+
         def strToInt(p):
             integer=0
             for i in p:
                 integer=(integer*10)+(ord(i)-ord('0'))
             return integer
-        return str(strToInt(num1)*strToInt(num2))
+        
+        def intToStr(p):
+            s=""
+            while(p):
+                r=p%10
+                p=p//10
+                s=chr(ord('0')+r)+s
+            return s
+            
+        return intToStr(strToInt(num1)*strToInt(num2))
     
                 
         
