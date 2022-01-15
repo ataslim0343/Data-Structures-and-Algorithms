be4af9d9bf0cd3740c5ca7cd97c975ec48295b0d
Md Taslim Ansari <ataslim0343@gmail.com>
2022-01-15 23:07:27 +0530

    Time: 22 ms (53.62%), Space: 13.2 MB (99.16%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if digits[-1]<9:
+            digits[-1]+=1
+            return digits
+        n=len(digits)
+        c=0
+        for i in range(n-1,-1,-1):
+            if digits[i]==9:
+                digits[i]=0
+                c+=1
+            else:
+                p=i
+                break
+        if(c==n):
+            return [1]+digits
+        else:
+            digits[p]+=1
+        return digits
