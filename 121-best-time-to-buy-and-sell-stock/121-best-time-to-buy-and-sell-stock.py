2ecbbb60a1822961606ee48e736ef7001c695de1
Md Taslim Ansari <ataslim0343@gmail.com>
2022-01-19 22:42:05 +0530

    Time: 1608 ms (9.07%), Space: 22.6 MB (77.59%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,9 +6,15 @@ class Solution(object):
         """
         minPrice=9999999
         maxProfit=0
-        for i in prices:
-            minPrice=min(minPrice,i)
-            maxProfit=max(maxProfit,i-minPrice)
-        return maxProfit
+#         for i in prices:
+#             minPrice=min(minPrice,i)
+#             maxProfit=max(maxProfit,i-minPrice)
+#         return maxProfit
             
+        for i in prices:
+            if(i<minPrice):
+                minPrice=i
+            if(maxProfit<i-minPrice):
+                maxProfit=i-minPrice
+        return maxProfit
         
