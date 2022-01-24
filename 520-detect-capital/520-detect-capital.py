3307b6736ba7359906a9c81d23db5dcf3c806d99
Md Taslim Ansari <ataslim0343@gmail.com>
2022-01-24 23:19:14 +0530

    Time: 33 ms (16.58%), Space: 13.6 MB (33.68%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -4,29 +4,7 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        def isLower(s):
-            c_lower=0
-            for i in s:
-                if ord(i)>=97 and ord(i)<=122:
-                    c_lower+=1
-            n=len(s)
-            if(n==c_lower):
-                return 1
-            return 0
-
-        def isUpper(s):
-            c_upper=0
-            for i in s:
-                if ord(i)>=65 and ord(i)<=90:
-                    c_upper+=1
-            n=len(s)
-            if(n==c_upper):
-                return 1
-            return 0
-        
-        
-        if(isUpper(word) or isLower(word) or (isUpper(word[0]) and isLower(word[1:]))):
+        if word.isupper() or word.istitle() or word.islower():
             return 1
-        else:
-            return 0
+        return 0
         
