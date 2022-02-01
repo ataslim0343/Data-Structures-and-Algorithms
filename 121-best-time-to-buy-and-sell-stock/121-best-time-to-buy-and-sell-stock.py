8c69e510e22fddb261a9b747d2cd8d48c5b0bee7
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-01 23:32:19 +0530

    Time: 932 ms (97.03%), Space: 25.1 MB (52.84%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         minPrice=9999999
         maxProfit=0
 #         for i in prices:
@@ -17,4 +13,3 @@ class Solution(object):
             if(maxProfit<i-minPrice):
                 maxProfit=i-minPrice
         return maxProfit
-        
