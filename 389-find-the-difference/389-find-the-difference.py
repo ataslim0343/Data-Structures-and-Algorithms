aa1ea298fe0ca60374dc33fcc5c4193dbc0531c4
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-02 22:58:03 +0530

    Time: 53 ms (31.97%), Space: 13.9 MB (98.71%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,10 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        c=0
-        for i in s:
-            c=c^ord(i)
-        for j in t:
-            c=c^ord(j)
-        return chr(c)
+        x=0
+        # for i in s:
+        #     c=c^ord(i)
+        # for j in t:
+        #     c=c^ord(j)
+        # return chr(c)
+        
+        for i in range(len(s)):
+            x+=ord(t[i])
+            x-=ord(s[i])
+        x+=ord(t[-1])
+        return chr(x)
+            
+            
+            
         
         
