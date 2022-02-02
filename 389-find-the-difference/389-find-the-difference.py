de3612c937fa614bdd822b5fee447d13ee8197cd
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-02 22:54:38 +0530

    Time: 65 ms (14.26%), Space: 13.9 MB (98.71%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        c=0
+        for i in s:
+            c=c^ord(i)
+        for j in t:
+            c=c^ord(j)
+        return chr(c)
+        
+        
