bdeac52d36f22fdd6d804654ceb39d121f643a72
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-09 23:40:42 +0530

    Time: 59 ms (58.74%), Space: 15.4 MB (85.26%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -8,7 +8,7 @@ class Solution:
                 d[i]=1
         sorted_dict=sorted(d.items(),key=lambda item:item[1],reverse=True)
         ans=""
-        print(sorted_dict)
+        #print(sorted_dict)
         for i, j in sorted_dict:
             ans+=i*j
         return ans
