f339009d0ffd6994ea7d523be463c5421149a05a
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-09 23:40:32 +0530

    Time: 71 ms (41.99%), Space: 15.4 MB (85.26%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,15 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        d={}
+        for i in s:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        sorted_dict=sorted(d.items(),key=lambda item:item[1],reverse=True)
+        ans=""
+        print(sorted_dict)
+        for i, j in sorted_dict:
+            ans+=i*j
+        return ans
+        
