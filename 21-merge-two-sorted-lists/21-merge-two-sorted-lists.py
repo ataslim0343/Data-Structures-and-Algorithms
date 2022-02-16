6f752891605366464ec86970321eb7c5431d7228
Md Taslim Ansari <ataslim0343@gmail.com>
2022-02-17 00:06:45 +0530

    Time: 36 ms (89.23%), Space: 13.9 MB (94.96%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -14,16 +14,16 @@ class Solution:
                 result.next=list2
                 list2=list2.next
             result=result.next
-        while(list1):
-                result.next=list1
-                list1=list1.next
-                result=result.next
-        while(list2):
-                result.next=list2
-                list2=list2.next
-                result=result.next
+        # while(list1):
+        #         result.next=list1
+        #         list1=list1.next
+        #         result=result.next
+        # while(list2):
+        #         result.next=list2
+        #         list2=list2.next
+        #         result=result.next
         
-        # result.next= list1 or list2
+        result.next= list1 or list2
         return head.next
                 
             
